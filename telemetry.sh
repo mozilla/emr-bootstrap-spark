28c98ca8037743338c07d9ef5473411c562e956e
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2015-08-20 14:52:45 +0100

    Switch to v4 as default schema.

--- telemetry.sh
+++ telemetry.sh
@@ -179,5 +179,4 @@ $(git clone --recursive https://github.com/vitillo/dotfiles.git;
 
 mkdir -p $HOME/analyses && cd $HOME/analyses
 wget https://raw.githubusercontent.com/vitillo/emr-bootstrap-spark/master/Telemetry%20Hello%20World.ipynb
-wget https://raw.githubusercontent.com/vitillo/emr-bootstrap-spark/master/Unified%20Telemetry-FHR%20Hello%20World.ipynb
 ipython notebook --browser=false&

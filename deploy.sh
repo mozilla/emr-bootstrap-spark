e5c610956fdc8722364f852b34981d61cc4f2424
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2015-10-20 15:55:24 -0400

    Move out from old-dev.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-aws s3 cp batch.sh s3://telemetry-spark-emr/batch.sh
-aws s3 cp telemetry.sh s3://telemetry-spark-emr/telemetry.sh
+aws s3 mb s3://telemetry-spark-emr-2
+
+aws s3 cp batch.sh s3://telemetry-spark-emr-2/batch.sh
+aws s3 cp telemetry.sh s3://telemetry-spark-emr-2/telemetry.sh

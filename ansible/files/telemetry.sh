50c7d6b048f5c64c33db91e2b76f6476d067e8f3
Wesley Dawson <whd@mozilla.com>
2016-02-29 15:48:52 -0800

    Enable EPEL (bug #1249651)

--- ansible/files/telemetry.sh
+++ ansible/files/telemetry.sh
@@ -3,6 +3,9 @@ MEMORY_OVERHEAD=7000  # Tuned for c3.4xlarge
 EXECUTOR_MEMORY=15000M
 DRIVER_MEMORY=$EXECUTOR_MEMORY
 
+# Enable EPEL
+yum-config-manager --enable epel
+
 # Install packages
 curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm.repo
 sudo yum -y install git jq htop tmux libffi-devel aws-cli postgresql-devel zsh snappy-devel readline-devel sbt

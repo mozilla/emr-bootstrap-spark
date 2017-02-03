cce30aa858b3330a3893baa30f11752072f794cf
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2017-02-03 15:52:23 +0000

    Refactor deployment playbook.

--- ansible/files/steps/zeppelin/zeppelin.sh
+++ ansible/files/steps/zeppelin/zeppelin.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-TELEMETRY_CONF_BUCKET=s3://telemetry-spark-emr-2
-
 # Configure Spark
 chmod o+w /mnt /mnt1
 source $HOME/.bashrc
 
 echo "export PYSPARK_PYTHON=$PYSPARK_PYTHON" | sudo tee -a /etc/zeppelin/conf/zeppelin-env.sh
-sudo aws s3 cp $TELEMETRY_CONF_BUCKET/configuration/zeppelin/interpreter.json /etc/zeppelin/conf/interpreter.json
+sudo aws s3 cp s3://{{telemetry_analysis_spark_emr_bucket}}/configuration/zeppelin/interpreter.json /etc/zeppelin/conf/interpreter.json
 sudo chown zeppelin:zeppelin /etc/zeppelin/conf/interpreter.json
 
 # Enable matplotlib support
